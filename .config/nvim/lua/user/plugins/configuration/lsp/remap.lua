48523a6753c6c3ef089dab4d9d25eb6f325546f1
wcs7777 <wcs777git@gmail.com>
2023-06-10 22:56:43 -0300

    refactor lsp remap in neovim

--- .config/nvim/lua/user/plugins/configuration/lsp/remap.lua
+++ .config/nvim/lua/user/plugins/configuration/lsp/remap.lua
@@ -1,4 +1,5 @@
 local create_buf_map = require('user.utils.map').create_buf_map
+local fn = require('user.utils.map').function_wrapper
 
 local function lsp_keymaps(buffer)
 	local lsp = vim.lsp.buf
@@ -16,21 +17,11 @@ local function lsp_keymaps(buffer)
 	buf_map('n', 'gr', lsp.references, 'All symbol references in quickfix')
 	buf_map('n', 'K', lsp.hover, 'Information about the symbol')
 	buf_map({ 'n', 'v' }, '<Leader>ca', lsp.code_action, 'Code Action')
-	buf_map('n', '<Leader>fmt', function()
-		lsp.format({ async = true })
-	end, 'Format all')
-	buf_map('n', '<Leader>dgn', function()
-		dgn.goto_next({ buffer = 0 })
-	end, 'Move to the next diagnostic')
-	buf_map('n', '[d', function()
-		dgn.goto_next({ buffer = 0 })
-	end, 'Move to the next diagnostic')
-	buf_map('n', '<Leader>dgp', function()
-		dgn.goto_prev({ buffer = 0 })
-	end, 'Move to the previous diagnostic')
-	buf_map('n', ']d', function()
-		dgn.goto_prev({ buffer = 0 })
-	end, 'Move to the previous diagnostic')
+	buf_map('n', '<Leader>fmt', fn(lsp.format, { async = true }), 'Format all')
+	buf_map('n', '<Leader>dgn', fn(dgn.goto_next, { buffer = 0 }), 'Move to the next diagnostic')
+	buf_map('n', '[d', fn(dgn.goto_next, { buffer = 0 }), 'Move to the next diagnostic')
+	buf_map('n', '<Leader>dgp', fn(dgn.goto_prev, { buffer = 0 }), 'Move to the previous diagnostic')
+	buf_map('n', ']d', fn(dgn.goto_prev, { buffer = 0 }), 'Move to the previous diagnostic')
 	buf_map('n', '<Leader>dof', dgn.open_float, 'Show diagnostics in a floating window')
 	buf_map('n', '<Leader>dsl', dgn.setloclist, 'Add buffer diagnostics to the location list')
 end
