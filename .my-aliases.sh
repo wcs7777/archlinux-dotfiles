72aaf3f84cea3dd03a2782849224ee131f0d377f
wcs7777 <wcs777git@gmail.com>
2023-06-28 11:12:46 -0300

    add mssql start alias

--- .my-aliases.sh
+++ .my-aliases.sh
@@ -8,6 +8,7 @@ alias hs='history 1 | rg'
 alias hsi='history 1 | rg -i'
 alias jsplayground="touch /tmp/tmp-playground.js && code /tmp/tmp-playground.js && node --watch /tmp/tmp-playground.js"
 alias lsall='ls -la --color=auto'
+alias mssql="sudo systemctl reset-failed && sudo systemctl start docker && docker container start mssql"
 alias permissions="stat -c '%a %U:%G %n'"
 alias py="pipenv run python"
 
