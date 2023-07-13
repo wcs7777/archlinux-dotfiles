cbe93c25241e85c381ba110498fa9deb8463a72b
wcs7777 <wcs777git@gmail.com>
2023-07-13 19:54:31 -0300

    my-setup: add tmux-plugins/tpm git clone

--- .my-setup/scripts/08-configure-zsh.sh
+++ .my-setup/scripts/08-configure-zsh.sh
@@ -5,3 +5,4 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git             ~/.
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions         ~/.zsh/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/jeffreytse/zsh-vi-mode.git            ~/.zsh/zsh-vi-mode
+git clone --depth=1 https://github.com/tmux-plugins/tpm                      ~/.tmux/plugins/tpm
