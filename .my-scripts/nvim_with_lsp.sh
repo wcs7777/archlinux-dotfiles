afa4063a722ff7e5b5902da0c8999c8d56ad0331
wcs7777 <wcs777git@gmail.com>
2023-07-13 19:54:03 -0300

    my-scripts: remove nvim_with_lsp.sh and nvim_without_lsp.sh

--- .my-scripts/nvim_with_lsp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ~/.my-scripts/with_lsp_plugins.lua ~/.config/nvim/lua/user/plugins/init.lua
-cp ~/.my-scripts/with_lsp_plugins_configuration.lua ~/.config/nvim/lua/user/plugins/configuration/init.lua
--- .my-scripts/nvim_without_lsp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ~/.my-scripts/without_lsp_plugins.lua ~/.config/nvim/lua/user/plugins/init.lua
-cp ~/.my-scripts/without_lsp_plugins_configuration.lua ~/.config/nvim/lua/user/plugins/configuration/init.lua
