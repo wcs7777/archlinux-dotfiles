501449c6a79c645e855f0b82c56ef9cdea5297da
wcs7777 <wcs777git@gmail.com>
2023-07-13 03:10:16 -0300

    nvim: change nvim-tree plugin config

--- .config/nvim/lua/user/plugins/configuration/navigation/nvim-tree.lua
+++ .config/nvim/lua/user/plugins/configuration/navigation/nvim-tree.lua
@@ -2,17 +2,33 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require('nvim-tree').setup({
-	disable_netrw = true,
-	sort_by = 'case_sensitive',
-	actions = {
-		open_file = {
-			resize_window = false,
-		},
+	filters = {
+		dotfiles = true,
 	},
-	view = {
-		width = '120',
-	},
-	renderer = {
+  disable_netrw = true,
+  hijack_netrw = true,
+  hijack_cursor = true,
+  hijack_unnamed_buffer_when_opening = false,
+  sync_root_with_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+  },
+  view = {
+    adaptive_size = false,
+    side = "left",
+    width = 30,
+    preserve_window_proportions = true,
+  },
+  filesystem_watchers = {
+    enable = true,
+  },
+  actions = {
+    open_file = {
+      resize_window = true,
+    },
+  },
+  renderer = {
 		icons = {
 			show = {
 				git = true,
@@ -39,8 +55,5 @@ require('nvim-tree').setup({
 			},
 		},
 		group_empty = true,
-	},
-	filters = {
-		dotfiles = true,
-	},
+  },
 })
