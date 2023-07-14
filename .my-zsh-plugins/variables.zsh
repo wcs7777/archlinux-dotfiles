7251a417d9ec075877d2662e9d2417840c4d5325
wcs7777 <wcs777git@gmail.com>
2023-07-13 22:05:48 -0300

    my-zsh-plugins: add variables.zsh

---
+++ .my-zsh-plugins/variables.zsh
@@ -0,0 +1,4 @@
+export SPARK_HOME="/mnt/wsl/shared/lib/spark"
+export PYTHONPATH="$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9.7-src.zip:$PYTHONPATH"
+export PATH="$PATH:$SPARK_HOME/bin"
+export PATH="$SPARK_HOME/python:$PATH"
