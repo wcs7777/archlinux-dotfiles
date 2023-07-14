5a7e223f59c748038f5a0493a1688f04c5f2435b
wcs7777 <wcs777git@gmail.com>
2023-07-14 13:36:01 -0300

    variables: add HADOOP_HOME

--- .my-zsh-plugins/variables.zsh
+++ .my-zsh-plugins/variables.zsh
@@ -1,4 +1,5 @@
 export SPARK_HOME="/mnt/wsl/shared/lib/spark"
+export HADOOP_HOME=$SPARK_HOME
 export PYTHONPATH="$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9.7-src.zip:$PYTHONPATH"
 export PATH="$PATH:$SPARK_HOME/bin"
 export PATH="$SPARK_HOME/python:$PATH"
