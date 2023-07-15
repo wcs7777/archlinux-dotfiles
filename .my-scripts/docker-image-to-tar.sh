fe563b42b25e20675b948a7fecda67dbe05290e0
wcs7777 <wcs777git@gmail.com>
2023-07-15 16:29:19 -0300

    my-scripts(docker-image-to-tar): add container name option

--- .my-scripts/docker-image-to-tar.sh
+++ .my-scripts/docker-image-to-tar.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
 __wcs_docker_image_to_tar() {
-	local distro
-	local outdir
-	local filename
-	local outfile
+	local image="${1:-archlinux}"
+	local container="${2:-$image}"
+	local outdir="${3:-.}"
+	local outfile="$outdir/$container.tar"
+	local image_created=false
+	local container_created=false
 	local image_id
 	local container_id
-	local image_created
-	local container_created
-	distro="${1:-archlinux}"
-	outdir="${2:-/mnt/d/wsl/dockerdistros}"
-	filename="${3:-$distro}.tar"
-	outfile="$outdir/$filename"
-	image_created=false
-	container_created=false
 	sudo systemctl reset-failed
 	sudo systemctl start docker
-	image_id=$(docker image ls --all | grep "\b${distro}\b" -m 1 | awk '{print $3}')
+	image_id=$(docker image ls --all | grep "\b${image}\b" -m 1 | awk '{print $3}')
 	if [ -z "$image_id" ]; then
-		docker pull "$distro"
-		image_id=$(docker image ls --all | grep "\b${distro}\b" -m 1 | awk '{print $3}')
+		docker pull "$image"
+		image_id=$(docker image ls --all | grep "\b${image}\b" -m 1 | awk '{print $3}')
 		image_created=true
 	fi
-	container_id=$(docker container ls --all | grep "\b${distro}\b" -m 1 | awk '{print $1}')
+	container_id=$(docker container ls --all | grep "\b${container}\b" -m 1 | awk '{print $1}')
 	if [ -z "$container_id" ]; then
-		container_id=$(docker create --name "$distro" -i "$distro")
+		container_id=$(docker container create --name "$container" --interactive "$image")
 		container_created=true
 	fi
 	mkdir -p "$outdir"
