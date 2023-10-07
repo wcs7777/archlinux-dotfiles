c56e22d9ad4684a05ed3218f7f7d9a442a417844
wcs7777 <wcs777git@gmail.com>
2023-10-07 15:04:31 -0300

    fix SPARK variables

--- .my/zsh-plugins/variables.zsh
+++ .my/zsh-plugins/variables.zsh
@@ -1,5 +1,6 @@
-export SPARK_HOME="/mnt/wsl/shared/lib/spark"
+# export SPARK_HOME="/mnt/wsl/shared/lib/spark-3.41"
+export SPARK_HOME="/mnt/wsl/shared/lib/spark-3.3.1-bin-hadoop3"
 export HADOOP_HOME=$SPARK_HOME
 export PYTHONPATH="$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9.7-src.zip:$PYTHONPATH"
 export PATH="$PATH:$SPARK_HOME/bin"
-export PATH="$SPARK_HOME/python:$PATH"
+export PATH="$PATH:$SPARK_HOME/python"
