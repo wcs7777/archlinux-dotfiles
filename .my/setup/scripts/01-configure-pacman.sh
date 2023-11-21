6e3e63955c0f6d9253e1753a362ac939876d8111
wcs7777 <wcs777git@gmail.com>
2023-11-21 10:45:43 -0300

    setup: add pacman-key --init fix comments

--- .my/setup/scripts/01-configure-pacman.sh
+++ .my/setup/scripts/01-configure-pacman.sh
@@ -2,6 +2,8 @@
 
 cp /etc/pacman.conf /etc/pacman.conf.old
 cp "$files_setup/pacman.conf" /etc/pacman.conf
+#killall gpg-agent
+#rm -rf /etc/pacman.d/gnupg/
 pacman-key --init
 pacman-key --populate
 pacman -Sy --noconfirm archlinux-keyring
