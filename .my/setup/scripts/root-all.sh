85066707277414a68638dc057c3ff255717fb6d5
wcs7777 <wcs777git@gmail.com>
2023-12-25 01:59:13 -0300

    setup: disable reinstall-packages

--- .my/setup/scripts/root-all.sh
+++ .my/setup/scripts/root-all.sh
@@ -2,6 +2,6 @@
 
 source "$scripts_setup/01-configure-pacman.sh"
 source "$scripts_setup/02-configure-locales.sh"
-source "$scripts_setup/03-reinstall-packages.sh"
+# source "$scripts_setup/03-reinstall-packages.sh"
 source "$scripts_setup/04-root-passwd.sh"
 source "$scripts_setup/05-create-user.sh"
