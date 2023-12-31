80e6fc1eec56e13991340ce9df85e1103c567fc5
wcs7777 <wcs777git@gmail.com>
2023-12-31 02:16:17 -0300

    setup: add yay first use commands

--- .my/setup/scripts/06-install-yay.sh
+++ .my/setup/scripts/06-install-yay.sh
@@ -3,3 +3,7 @@
 sudo pacman -S --needed --noconfirm git base-devel && \
 git clone --depth=1 https://aur.archlinux.org/yay.git/ /tmp/yay && \
 (cd /tmp/yay && makepkg -si --noconfirm)
+yay -Y --gendb --noconfirm
+yay -Syu --devel --noconfirm
+yay -Y --devel --save --noconfirm
+yay -Yc
