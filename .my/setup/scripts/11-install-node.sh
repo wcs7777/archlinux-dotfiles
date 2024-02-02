6e36a97c925e4774b85537eea7b6180c0ab2bf10
wcs7777 <wcs777git@gmail.com>
2024-02-02 01:15:29 -0300

    setup: update nodejs version to 20.11.0

--- .my/setup/scripts/11-install-node.sh
+++ .my/setup/scripts/11-install-node.sh
@@ -3,5 +3,5 @@
 . /opt/asdf-vm/asdf.sh
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf list all nodejs
-asdf install nodejs 20.10.0
-asdf global nodejs 20.10.0
+asdf install nodejs 20.11.0
+asdf global nodejs 20.11.0
