a2a2d5c7fa8eeda2103699bb478c2cd5a787ceb3
wcs7777 <wcs777git@gmail.com>
2024-02-02 01:24:11 -0300

    scripts: installation of azure tools

---
+++ .my/scripts/install-azure-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. /opt/asdf-vm/asdf.sh
+
+readonly azure_cli_version="2.56.0"
+readonly azure_func_version="4.0.5455"
+
+asdf plugin add azure-cli https://github.com/EcoMind/asdf-azure-cli
+asdf list all azure-cli
+asdf install azure-cli $azure_cli_version
+asdf global azure-cli $azure_cli_version
+
+asdf plugin add azure-functions-core-tools https://github.com/daveneeley/asdf-azure-functions-core-tools.git
+asdf list all azure-functions-core-tools
+asdf install azure-functions-core-tools $azure_func_version
+asdf global azure-functions-core-tools $azure_func_version
