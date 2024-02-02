fec25ba031a3e4dea6a96c4da2320b96026dd626
wcs7777 <wcs777git@gmail.com>
2024-02-02 01:23:44 -0300

    setup: use variable to define node version

--- .my/setup/scripts/11-install-node.sh
+++ .my/setup/scripts/11-install-node.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 . /opt/asdf-vm/asdf.sh
+
+readonly version="20.11.0"
+
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf list all nodejs
-asdf install nodejs 20.11.0
-asdf global nodejs 20.11.0
+asdf install nodejs $version
+asdf global nodejs $version
