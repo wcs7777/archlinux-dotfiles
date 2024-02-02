37020e691684d14b68fcf722aca2969e03acce91
wcs7777 <wcs777git@gmail.com>
2024-02-02 01:12:45 -0300

    setup: refactor indentation

--- .my/setup/scripts/12-vscode-extensions.sh
+++ .my/setup/scripts/12-vscode-extensions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#code --install-extension vscjava.vscode-java-pack
-#code --install-extension pivotal.vscode-boot-dev-pack
-#code --install-extension redhat.fabric8-analytics
+# code --install-extension vscjava.vscode-java-pack
+# code --install-extension pivotal.vscode-boot-dev-pack
+# code --install-extension redhat.fabric8-analytics
 # code --install-extension ritwickdey.liveserver
 # code --install-extension maptz.camelcasenavigation
 code --install-extension streetsidesoftware.code-spell-checker
 # code --install-extension mrmlnc.vscode-postcss-sorting
-#code --install-extension wmaurer.change-case
+# code --install-extension wmaurer.change-case
 # code --install-extension bbugh.change-color-format
