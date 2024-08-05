256cf68654c3ce03d8dfeb1af7b00f3b18f48975
wcs7777 <wcs777git@gmail.com>
2024-08-05 02:59:26 -0300

    neovim(whick-key): update configs to v3

--- .config/nvim/lua/user/plugins/configuration/util/which-key.lua
+++ .config/nvim/lua/user/plugins/configuration/util/which-key.lua
@@ -1,6 +1,6 @@
 require('which-key').setup({
-	spelling = { enabled = true },
+	preset = 'modern',
+	delay = 900,
+	notify = true,
 	show_help = false,
-	triggers_nowait = {},
 })
-vim.opt.timeoutlen = 900
