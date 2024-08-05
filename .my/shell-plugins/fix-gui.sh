317b8c05fbcdd02e8f81c30a6fb0388f692b766d
wcs7777 <wcs777git@gmail.com>
2024-08-05 02:56:39 -0300

    shell-plugins: improve fix-gui.sh

--- .my/shell-plugins/fix-gui.sh
+++ .my/shell-plugins/fix-gui.sh
@@ -4,8 +4,10 @@ file_guard='/tmp/gui-fixed'
 
 if ! test -f $file_guard; then
 	sudo umount --quiet /tmp/.X11-unix
-	sudo rm -rf /tmp/.X11-unix
-	sudo ln -s /mnt/wslg/.X11-unix /tmp/.X11-unix
-	sudo ln -s /mnt/wslg/runtime-dir/wayland-0* /run/user/1000/
+	sudo rm --recursive --force /tmp/.X11-unix
+	sudo ln --symbolic /mnt/wslg/.X11-unix /tmp/.X11-unix
+	sudo ln --symbolic --force \
+		/mnt/wslg/runtime-dir/wayland-0* \
+		/run/user/1000/
 	sudo touch $file_guard
 fi
